b4ee4aa1178b1a34d88e28a23a897ec4fbf183e7
Mohamed Benabdellah <77970397+Pooreffects@users.noreply.github.com>
2024-08-22 23:44:09 +0100

    feat: Drag and Drop functionalities

--- src/components/BurnBarrel.tsx
+++ src/components/BurnBarrel.tsx
@@ -9,8 +9,29 @@ interface BurnBarrelProps {
 
 export default function BurnBarrel({ setCards }: BurnBarrelProps) {
   const [active, setActive] = useState(false);
+
+  /* Handle Drag Functionalities */
+  function handleDragOver(e: React.DragEvent) {
+    e.preventDefault();
+    setActive(true);
+  }
+
+  function handleDragLeave() {
+    setActive(false);
+  }
+
+  function handleDragEnd(e: React.DragEvent) {
+    const cardId = e.dataTransfer.getData('cardId');
+
+    setCards((prev) => prev.filter((card) => card.id !== cardId));
+    setActive(false);
+  }
+
   return (
     <div
+      onDrop={handleDragEnd}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
       className={`mt-10 grid h-56 w-56 shrink-0 place-content-center rounded border text-3xl ${
         active
           ? 'border-red-800 bg-red-800/20 text-red-500'
